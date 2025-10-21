d2e7de4d52cf3e741b84d31fd54636642acef1e4
Pedro <pedropauloade@gmail.com>
2025-10-21 12:18:52 -0300

    First component

---
+++ FirstProject/src/components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+const FirstComponent = () => {
+
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+            <p>Meu texto</p>
+        </div>
+    );
+
+};
+
+export default FirstComponent;
